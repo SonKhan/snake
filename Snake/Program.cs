76d0da654b73749876c61a104d73ede6b35eb749
4web <avicenna10000@gmail.com>
2021-10-07 16:16:34 +0300

    Lesson6\Class "Список"(List<>)

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Snake
 {
@@ -7,36 +11,46 @@ namespace Snake
         static void Main(string[] args)
         {
             Point p1 = new Point(1,3,'*');
-            /*p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';*/
             p1.Draw();
 
             Point p2 = new Point(4,5,'#');
-            /*p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';*/
             p2.Draw();
 
-            /*int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
+            Point p3 = new Point(8, 11, '%');
+            p3.Draw();
 
-            Draw(x1, y1, sym1);*/
+            Point p4 = new Point(14, 15, '&');
+            p4.Draw();
 
-            /*int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
 
-            Draw(x2, y2, sym2);*/
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+            List<char> sList = new List<char>();
+            sList.Add('@');
+            sList.Add('#');
+            sList.Add('$');
+            sList.Add('%');
 
             Console.ReadLine();
         }
-        /* static void Draw(int x, int y, char sym)
-         {
-             Console.SetCursorPosition(x, y);
-             Console.Write(sym);
-         }*/
-
     }
 }
